11243c773de36fbc520b1c9485bf45ecb0b3b4e1
CelesteMagisteel <kamg1134@gmail.com>
2020-08-19 15:34:17 +0100

    Added more placeholders

--- src/main/java/net/sapphirehollow/marriage/PlaceholderAPIHook.java
+++ src/main/java/net/sapphirehollow/marriage/PlaceholderAPIHook.java
@@ -3,11 +3,13 @@ package net.sapphirehollow.marriage;
 import me.clip.placeholderapi.expansion.PlaceholderExpansion;
 import net.sapphirehollow.marriage.controllers.MarriageController;
 import net.sapphirehollow.marriage.storage.PlayerStorage;
+import org.bukkit.ChatColor;
 import org.bukkit.OfflinePlayer;
 import org.bukkit.entity.Player;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 public class PlaceholderAPIHook extends PlaceholderExpansion {
 
@@ -43,18 +45,30 @@ public class PlaceholderAPIHook extends PlaceholderExpansion {
             case "heart": return storage.getChatPrefix(false);
             case "married_list": return uuidListToNames(storage.getMarriages());
             case "married_list_full":
-                if (storage.getMarriages().size() == 0) return "";
-                return "Marriages: " + uuidListToNames(storage.getMarriages());
+                if (storage.getEngagements().size() == 0) return "";
+                return ChatColor.GOLD + "Engagements: " + ChatColor.YELLOW + uuidListToNames(storage.getMarriages());
             case "married_list_full_new":
-                if (storage.getMarriages().size() == 0) return "";
-                return "\nMarriages: " + uuidListToNames(storage.getMarriages());
+                if (storage.getEngagements().size() == 0) return "";
+                return ChatColor.GOLD + "Engagements: " + ChatColor.YELLOW + uuidListToNames(storage.getMarriages()) + "\n";
+            case "new_married_list_full":
+                if (storage.getEngagements().size() == 0) return "";
+                return "\n" + ChatColor.GOLD + "Engagements: " + ChatColor.YELLOW + uuidListToNames(storage.getMarriages());
+            case "new_married_list_full_new":
+                if (storage.getEngagements().size() == 0) return "";
+                return "\n" + ChatColor.GOLD + "Engagements: " + ChatColor.YELLOW + uuidListToNames(storage.getMarriages()) + "\n";
             case "engaged_list": return uuidListToNames(storage.getEngagements());
             case "engaged_list_full":
                 if (storage.getEngagements().size() == 0) return "";
-                return "Engagements: " + uuidListToNames(storage.getEngagements());
+                return ChatColor.GOLD + "Engagements: " + ChatColor.YELLOW + uuidListToNames(storage.getEngagements());
             case "engaged_list_full_new":
                 if (storage.getEngagements().size() == 0) return "";
-                return "\nEngagements: " + uuidListToNames(storage.getEngagements());
+                return ChatColor.GOLD + "Engagements: " + ChatColor.YELLOW + uuidListToNames(storage.getEngagements()) + "\n";
+            case "new_engaged_list_full":
+                if (storage.getEngagements().size() == 0) return "";
+                return "\n" + ChatColor.GOLD + "Engagements: " + ChatColor.YELLOW + uuidListToNames(storage.getEngagements());
+            case "new_engaged_list_full_new":
+                if (storage.getEngagements().size() == 0) return "";
+                return "\n" + ChatColor.GOLD + "Engagements: " + ChatColor.YELLOW + uuidListToNames(storage.getEngagements()) + "\n";
             default: return "";
         }
     }
@@ -63,7 +77,7 @@ public class PlaceholderAPIHook extends PlaceholderExpansion {
         StringBuilder string = new StringBuilder();
         for (int i = 0; i < uuids.size(); i++) {
             String rawId = uuids.get(i);
-            OfflinePlayer player = Marriage.getStorageController().getOfflinePlayer(rawId);
+            OfflinePlayer player = Marriage.instance.getServer().getOfflinePlayer(UUID.fromString(rawId));
             string.append(player.getName());
             if (i < uuids.size() - 1) string.append(", ");
         }
